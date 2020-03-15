a9515ab7020db4a0a25efe72d5aa491d272a46c1
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-15 17:35:39 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/lutfiqaraman-paymentsystem/payment'));
+app.use(express.static(__dirname + '/lutfiqaraman-paymentsystem'));
 
 app.get('/*', function(req,res) {
     
