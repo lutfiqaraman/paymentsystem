608758a080d1861f2ac1b841658ab7473d050c44
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-15 19:53:20 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname, 'dist', {index: false}));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname +'/dist/index.html'));
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 // Start the app by listening on the default Heroku port
