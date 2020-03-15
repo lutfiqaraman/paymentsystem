a99a3f70fc67019acbf79238d13a8ead6825cd74
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-15 17:22:21 +0200

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCvjkhoW46gHjrUg0fs0SB_gFK_uE9XSBc',
+    authDomain: 'pscrud.firebaseapp.com',
+    databaseURL: 'https://pscrud.firebaseio.com',
+    projectId: 'pscrud',
+    storageBucket: 'pscrud.appspot.com',
+    messagingSenderId: '998585259994',
+    appId: '1:998585259994:web:3d16560ddbc60a756c52e3',
+    measurementId: 'G-DF042MMCQY'
+  }
 };
