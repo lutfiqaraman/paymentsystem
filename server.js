9d997f4c45f01d18c4c0eb09bb2e317ccafa4499
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-15 20:08:51 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname, 'dist', {index: false}));
+app.use(express.static(__dirname + '/dist/payment'));
 
 app.get('', function(req, res) {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
