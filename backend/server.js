ea074d40161d168770339cd8f62cdb9182f5f581
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-06 21:53:39 +0300

    edit server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3000);
